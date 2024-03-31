c4af3872cddbb645ac2bb14c055787ee7e274989
lepszypiter <96298134+lepszypiter@users.noreply.github.com>
2024-03-31 17:15:15 +0200

    GetAllTags first version

--- MediportaTest/Repositories/TagsRepository.cs
+++ MediportaTest/Repositories/TagsRepository.cs
@@ -1,11 +1,23 @@
+using MediportaTest.Context;
 using MediportaTest.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MediportaTest.Repositories;
 
 class TagsRepository : ITagsRepository
 {
-    public Task<IReadOnlyCollection<Tag>> GetAllTags()
+    private readonly TagsDBContext _context;
+    private readonly ILogger _logger;
+    public TagsRepository(TagsDBContext context, ILogger<TagsRepository> logger)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _logger = logger;
     }
+    public async Task<IReadOnlyCollection<Tag>> GetAllTags()
+    {
+        var result = await _context.Tags.Take(15).ToListAsync();
+        _logger.LogTrace("GetAllTags {Count}",  result.Count);
+        return result;
+    }
+    
 }
