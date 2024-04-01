2ebf0d3888f439ece89f4ba76b0f8e404dd99706
lepszypiter <96298134+lepszypiter@users.noreply.github.com>
2024-04-01 20:28:45 +0200

    Add percent field to tag table

--- MediportaTest/Migrations/20240401182733_Add percent field.Designer.cs
+++ MediportaTest/Migrations/20240401182733_Add percent field.Designer.cs
@@ -2,6 +2,7 @@
 using MediportaTest.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MediportaTest.Migrations
 {
     [DbContext(typeof(TagsDBContext))]
-    partial class TagsDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240401182733_Add percent field")]
+    partial class Addpercentfield
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.1");
--- MediportaTest/Models/Tag.cs
+++ MediportaTest/Models/Tag.cs
@@ -5,4 +5,5 @@ public class Tag
     public long Id { get; set; }
     public string Name { get; set;}
     public long Count { get; set;}
+    public decimal Percent { get; set; }
 }
